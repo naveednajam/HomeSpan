8671b814910308ce1837dc78af51ac975ff5f85f
naveednajam <naveed.najam@gmail.com>
2021-01-08 14:33:22 +0800

    Update WebOta.h

--- src/WebOta.h
+++ src/WebOta.h
@@ -6,3 +6,8 @@
 #include <Update.h>
 
 Webserver updateServer(8888);
+
+const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+
+void setupOTA(void);
+void webotaloop(void);
